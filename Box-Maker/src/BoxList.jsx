8f5a23305fbbe72f9ca4e4244d6b1b340c704e5f
Kyamran Rizayev <kyamranr@gmail.com>
2024-10-28 21:50:27 -0500

    removed unnecessary code and created BoxList component

---
+++ Box-Maker/src/BoxList.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+const BoxList = () => {
+  const [boxes, setBoxes] = useState([]);
+
+  const addBox = (box) => {
+    setBoxes((boxes) => [...boxes, box]);
+  };
+
+  const removeBox = (id) => {
+    setBoxes((boxes) => boxes.filter((box) => box.id !== id));
+  };
+
+  return (
+    <div>
+      <NewBoxForm addBox={addBox} />
+      <div>
+        {boxes.map((box) => (
+          <Box
+            key={box.id}
+            id={box.id}
+            width={box.width}
+            height={box.height}
+            backgroundColor={box.backgroundColor}
+            removeBox={removeBox}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BoxList;
