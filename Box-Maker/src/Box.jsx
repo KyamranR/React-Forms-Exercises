477c2da893c053d96bb591a0757c1f31b52ebcf1
Kyamran Rizayev <kyamranr@gmail.com>
2024-10-28 21:56:19 -0500

    created Box componant

---
+++ Box-Maker/src/Box.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Box = ({ id, width, height, backgroundColor, removeBox }) => {
+  const handleRemove = () => removeBox(id);
+
+  return (
+    <div>
+      <div
+        style={{ width: `${width}px`, height: `${height}px`, backgroundColor }}
+      ></div>
+      <button onClick={handleRemove}>X</button>
+    </div>
+  );
+};
+
+export default Box;
